13126091d0e0b9cf21cc34ef696063474a44505b
mhmdali102 <mhmdali102@bk.ru>
2022-12-03 21:46:07 +0200

    added hero buttons

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -6,6 +6,10 @@ import { useState } from "react";
 
 const navLinks = [
   {
+    href: "#hero",
+    text: "Home",
+  },
+  {
     href: "#projects",
     text: "Projects",
   },
@@ -36,7 +40,10 @@ const Header = () => {
   return (
     <header className="container rounded-b-lg bg-crust fixed z-10 top-0 left-0 right-0 mx-auto">
       <div className="flex items-center justify-between gap-4 flex-col md:flex-row">
-        <Branding setNavVisibility={setNavVisibility} navVisibility={navVisibility} />
+        <Branding
+          setNavVisibility={setNavVisibility}
+          navVisibility={navVisibility}
+        />
         <List items={navLinks} visible={navVisibility} col />
         <List items={icons} visible={navVisibility} />
       </div>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,12 +1,36 @@
+import Link from "next/link";
 import AnimatedText from "./AnimatedText";
+import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section className="pt-28 flex items-center relative overflow-hidden" style={{minHeight: "calc(15vw + 45vh)"}}>
-      <div className="container w-fit md:w-auto my-auto">
-        <h3>Hello👋 I&apos;m <span className="font-bold">MAHcodes</span></h3>
-        <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-lavender pt-4 pb-8">Frontend Developer</h1>
-        <p className="text-xl">I craft <span className="font-bold">AWESOME</span> websites with ️❤️</p>
+    <section
+      id="hero"
+      className="pt-28 flex items-center relative overflow-hidden"
+      style={{ minHeight: "calc(15vw + 45vh)" }}
+    >
+      <div className="container w-fit md:w-auto my-auto text-text">
+        <h3>
+          Hello👋 I&apos;m <span className="font-bold">MAHcodes</span>
+        </h3>
+        <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-lavender pt-4 pb-8">
+          Frontend Developer
+        </h1>
+        <p className="text-xl mb-8">
+          I craft <span className="font-bold">AWESOME</span> websites with ️❤️
+        </p>
+        <div className="flex items-center gap-2">
+          <Link href="#projects">
+            <Button variant="primary" size="lg">
+              Projects
+            </Button>
+          </Link>
+          <Link href="#contact">
+            <Button variant="secondary" size="lg">
+              Contact
+            </Button>
+          </Link>
+        </div>
       </div>
       <AnimatedText />
     </section>
