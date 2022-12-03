30d023feb993c18e60840ffcf900ba4b807a5cbf
mhmdali102 <mhmdali102@bk.ru>
2022-12-03 13:27:33 +0200

    animate hover colors

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 import Logo from "./icons/Logo";
 
 const Header = () => (
-  <header className="container py-6 rounded-b-lg bg-base">
+  <header className="container py-6 rounded-b-lg bg-crust">
     <div className="px-2">
       <Link href="/">
-        <Logo fill="fill-text" stroke="stroke-text" />
+        <Logo className="group" fill="fill-text group-hover:fill-lavender" stroke="stroke-text group-hover:stroke-lavender" />
       </Link>
     </div>
   </header>
