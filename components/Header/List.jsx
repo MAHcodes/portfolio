2ab775bb80b1672917a45ae21184964970c71db1
mhmdali102 <mhmdali102@bk.ru>
2022-12-04 01:09:05 +0200

    added icons

--- components/Header/List.jsx
+++ components/Header/List.jsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
 const List = ({ items, col, visible, setVisible }) => (
-  <ul className={`${col ? "flex-col md:flex-row" : null} ${visible ? "flex" : "hidden md:flex"} items-center`}>
+  <ul className={`${col ? "flex-col md:flex-row" : ""} ${visible ? "flex" : "hidden md:flex"} items-center`}>
     {items.map((item, idx) => (
       <li
         key={item.href}
-        className={`group border-b-transparent border-b-2 hover:text-lavender text-text ${col ? null : "hover:border-b-current" } md:hover:border-b-current translate-x-8 opacity-0 animate-fadeIn`}
+        className={`group border-b-transparent border-b-2 hover:text-lavender text-text ${col ? "" : "hover:border-b-current" } md:hover:border-b-current translate-x-8 opacity-0 animate-fadeIn`}
         style={{animationDelay: `${idx * 100}ms`}}
         onClick={() => setVisible(false)}
       >
