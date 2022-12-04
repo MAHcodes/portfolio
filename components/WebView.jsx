a0cdf886a42d39686fa27d2dd228516e22f0161e
mhmdali102 <mhmdali102@bk.ru>
2022-12-04 14:19:52 +0200

    added webview

---
+++ components/WebView.jsx
@@ -0,0 +1,24 @@
+const WebView = ({ alt, src, url }) => (
+  <div className="flex-04 relative overflow-y-auto aspect-video rounded-md border-2 border-text">
+    <div className="sticky top-0 bg-base flex gap-2 px-2 py-1 items-center border-b-text border">
+      <div className="flex items-center gap-1 min-w-max">
+        <Btn bgColor="#FF605C" />
+        <Btn bgColor="#FFBD44" />
+        <Btn bgColor="#00CA4E" />
+      </div>
+      <div className="bg-crust text-sm py-1 px-2 rounded-md w-full">
+        <span className="text-overlay0">https://</span>
+        {url.replace("https://", "")}
+      </div>
+    </div>
+    <div className="">
+      <img alt={alt} className="w-full" src={src} />
+    </div>
+  </div>
+);
+
+const Btn = ({ bgColor }) => (
+  <div className="w-3 h-3 rounded-full" style={{ backgroundColor: bgColor }} />
+);
+
+export default WebView;
