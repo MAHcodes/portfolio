af0b47ce9e696f9e14525a2a930dcbb5b97afe24
mhmdali102 <mhmdali102@bk.ru>
2022-12-04 23:15:42 +0200

    fix visit mah.codes sites

--- components/WorkButtons.jsx
+++ components/WorkButtons.jsx
@@ -2,23 +2,25 @@ import Button from "./Button";
 import GitHub from "./icons/GitHub";
 import ExternalLink from "./icons/ExternalLink";
 
-const WorkButtons = ({url, sourceCode}) => (
+const WorkButtons = ({ url, sourceCode }) => (
   <div className="flex items-center pt-4 gap-2 flex-col sm:flex-row">
-    <a
-      href={url}
-      target="_blank"
-      rel="noreferrer noopener"
-      className="w-full"
-    >
-      <Button
-        variant="primary"
-        size="lg"
-        className="w-full bg-lavender"
-        icon={<ExternalLink fill="fill-base" />}
+    {url !== "https://mah.codes" ? (
+      <a
+        href={url}
+        target="_blank"
+        rel="noreferrer noopener"
+        className="w-full"
       >
-        Visit
-      </Button>
-    </a>
+        <Button
+          variant="primary"
+          size="lg"
+          className="w-full bg-lavender"
+          icon={<ExternalLink fill="fill-base" />}
+        >
+          Visit
+        </Button>
+      </a>
+    ) : null}
     <a
       href={sourceCode}
       target="_blank"
